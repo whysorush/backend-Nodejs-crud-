42668d9320dcbace612a9f2c10daf08475b2ff4a
Rushabh.shah <rushabh.shah@gupshup.io>
2024-06-12 02:06:44 +0530

    crud opp v1.1

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 export const PORT = 5555;
-export const mongoDbURL = "mongodb+srv://rushabhshah5517:mcJcZwRK9i7PIFco@book-store-mern.2cwedcp.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-Store-Mern";
+export const mongoDbURL = //Mongo url
 // rushabhshah5517
 // mcJcZwRK9i7PIFco
